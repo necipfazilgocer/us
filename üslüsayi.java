04863d10ac98cd1f12ac6f4af9a153d32ae29cc8
necipfazilgocer <114177524+necipfazilgocer@users.noreply.github.com>
2022-09-28 21:27:44 +0300

    Add files via upload

---
+++ üslüsayi.java
@@ -0,0 +1,29 @@
+package üslüsayi;
+import java.util.Scanner;
+public class üslüsayi {
+static int result = 1;
+	    static int us(int x, int y){
+	        if (x == 0){
+	            return 1;
+	        }
+
+	        result *= y;
+	        us(x-1,y);
+	        return result;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+	        int x, y;
+
+	        System.out.println("Tabandaki sayiyi giriniz: ");
+	        y = input.nextInt();
+	        System.out.println("Usteki sayiyi giriniz: ");
+	        x = input.nextInt();
+
+	        System.out.println(us(x,y));
+	    }
+	
+	}
+
+
